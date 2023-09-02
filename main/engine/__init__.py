28be2b8322fb158af07e73f2582b6b240179bcbb
LioQing <46854695+LioQing@users.noreply.github.com>
2023-09-02 21:39:50 +0800

    Reorganize and add BaseComponent and BaseButton

--- main/engine/__init__.py
+++ main/engine/__init__.py
@@ -1,6 +1,27 @@
 import logging
-
+import numpy as np
 import pygame
+from pydantic import BaseModel, Field
+
+import main.engine.components as components
+
+
+class EngineSettings(BaseModel):
+    """Settings for the engine."""
+
+    fps: int = Field(60)
+    display: int = Field(0)
+    vsync: bool = Field(True)
+
+
+class Layer(pygame.sprite.RenderUpdates):
+    """A layer of sprites to render."""
+
+    priority: int
+
+    def __init__(self, priority: int, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.priority = priority
 
 
 class Engine:
@@ -8,39 +29,41 @@ class Engine:
 
     display: pygame.SurfaceType
     running: bool = False
-    _layers: dict[str, pygame.sprite.RenderUpdates]
-    _layer_priorities = list[tuple[int, str]]
     logger: logging.Logger
     clock: pygame.time.Clock
-    fps: int = 60
+    settings: EngineSettings
 
-    def __init__(self):
-        self.logger = logging.getLogger("engine")
+    _layers: dict[str, Layer]  # name: layer
+
+    def __init__(self, settings: EngineSettings = EngineSettings()):
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.INFO)
         self.logger.info("Starting engine!")
+
+        self.settings = settings
 
         # Provide a list of desktops (for fullscreen, use display.Info() instead)
         display_info = pygame.display.get_desktop_sizes()
         self.logger.info("Available displays: %s", display_info)
 
-        selected_display_size = display_info[0]
+        selected_display_size = display_info[settings.display]
         self.logger.info("Selected display: %s", selected_display_size)
 
         # For testing and debugging, default to 2/3 size
         window_size = (selected_display_size[0] / 3 * 2), (selected_display_size[1] / 3 * 2)
         self.logger.info("Creating a window with size %s", window_size)
 
-        self.display = pygame.display.set_mode(window_size, vsync=True)
+        self.display = pygame.display.set_mode(window_size, vsync=settings.vsync)
 
         # Do not pass fps here, as this clock is multi-use
         self.clock = pygame.time.Clock()
 
         self._layers = {}
-        self._layer_priorities = []
 
     @property
-    def layers(self):
+    def layers(self) -> list[tuple[str, Layer]]:
         """A list of (name, layer) for the engine's render layers in priority sorted order"""
-        return ((name, self._layers[name]) for _, name in self._layer_priorities)
+        return list(sorted(self._layers.items(), key=lambda item: item[1].priority))
 
     def add_layer(
         self,
@@ -54,16 +77,18 @@ class Engine:
         The default for priority is below all previously assigned layers.
         """
         self.logger.debug("Adding new layer '%s' at priority %s", name, "default" if priority is None else priority)
-        if priority is None:
-            priority = max(self._layer_priorities.values(), default=0) + 1
 
-        if name in self._layers.keys():
+        # Generate a priority if none is provided
+        if priority is None:
+            if len(self.layers) == 0:
+                priority = 0
+            else:
+                priority = self.layers[-1][1].priority + 1
+
+        if name in self._layers:
             raise ValueError("Duplicate layer name")
 
-        self._layers[name] = layer
-
-        self._layer_priorities.append((priority, name))
-        self._layer_priorities.sort()
+        self._layers[name] = Layer(priority=priority, *layer)
 
     def add_sprite(self, layer: str, *sprites: pygame.sprite.DirtySprite):
         """Add a sprite to a render layer."""
@@ -82,14 +107,14 @@ class Engine:
     def update(self, events: list[pygame.Event]):
         """Update all sprites and layers with events that have occurred since last call."""
         for event in events:
-            print(event)
+            self.logger.debug(event)
             if event.type == pygame.QUIT:
                 self.running = False
 
-        tick_time = self.clock.tick(self.fps) / 1000
+        delta_time = self.clock.tick(self.settings.fps) / 1000  # in seconds
         for name, layer in self.layers:
             self.logger.debug("Updating layer %s", name)
-            layer.update(tick_time, events)
+            layer.update(delta_time, events)
 
     def draw(self) -> list[pygame.Rect]:
         """Move all sprites and rerender all layers."""
@@ -106,6 +131,10 @@ class Engine:
         """Start the engine."""
         self.running = True
 
+        # Test
+        self.add_layer("test button", pygame.sprite.RenderUpdates())
+        self.add_sprite("test button", TestButton())
+
         while self.running:
             self.update(pygame.event.get())
             dirty_rects = self.draw()
@@ -113,3 +142,40 @@ class Engine:
 
         del self.display
         pygame.quit()
+
+
+# TODO: Remove this
+class TestButton(components.BaseButton):
+    """A count button."""
+
+    count: int = 0
+
+    def __init__(self):
+        super().__init__()
+        self.set_size((200, 50))
+        self.set_position((100, 100))
+
+        self.render_text()
+
+    def render_text(self):
+        """Render the text."""
+        surface = np.ndarray((200, 50, 4), dtype=np.uint8)
+        surface.fill(255)
+
+        # Use pygame text for testing
+        font = pygame.font.SysFont("Arial", 40)
+        text = font.render(str(self.count), True, (0, 0, 0)).convert_alpha()
+        text_surface = pygame.surfarray.pixels3d(text)
+
+        # if alpha of text is 0, set text_surface of the pixel to 255, 255, 255
+        text_surface[pygame.surfarray.pixels_alpha(text)[:, :] == 0] = 255
+
+        surface[:text.get_width(), :text.get_height(), :3] = text_surface
+
+        self.set_surface(surface)
+
+    def on_click(self):
+        """Called when the button is clicked."""
+        self.count += 1
+        print(self.count)
+        self.render_text()
---
+++ main/engine/components.py
@@ -0,0 +1,102 @@
+import pygame
+import abc
+import numpy
+import numpy.typing
+
+
+class BaseComponent(abc.ABC, pygame.sprite.DirtySprite):
+    """A sprite that can be drawn by the engine."""
+
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.Surface((0, 0)).convert_alpha()
+        self.rect = self.image.get_rect()
+
+    @property
+    def position(self) -> tuple[int, int]:
+        """The x coordinate of the component."""
+        return self.rect.x, self.rect.y
+
+    @property
+    def size(self) -> tuple[int, int]:
+        """The width of the component."""
+        return self.rect.width, self.rect.height
+
+    @property
+    def center(self) -> tuple[int, int]:
+        """The center of the component."""
+        return self.position[0] + self.size[0] // 2, self.position[1] + self.size[1] // 2
+    
+    @property
+    def surface(self) -> numpy.typing.NDArray[numpy.uint8]:
+        """The surface of the component."""
+        return pygame.surfarray.pixels3d(self.image)
+
+    def set_position(self, position: tuple[int, int]):
+        """Set the position of the component."""
+        self.rect.x = position[0]
+        self.rect.y = position[1]
+
+    def set_size(self, size: tuple[int, int]):
+        """Set the size of the component."""
+        self.rect.width = size[0]
+        self.rect.height = size[1]
+
+    def set_surface(self, surface: numpy.typing.NDArray[numpy.uint8]):
+        """
+        Set the surface of the component.
+
+        If rgba is True, the surface will be converted to an alpha surface.
+        """
+        if surface.shape[:2] != self.size:
+            raise ValueError(f"Surface size {surface.shape[:2]} does not match component size {self.size}.")
+
+        self.image = make_surface_rgba(surface)
+
+
+class BaseButton(BaseComponent):
+    """Component of the UI"""
+    
+    is_down: bool
+
+    def __init__(self):
+        super().__init__()
+        self.is_down = False
+
+    @abc.abstractmethod
+    def on_click(self):
+        """Called when the button is clicked."""
+        pass
+
+    def update(self, delta_time: float, events: list[pygame.event.Event]):
+        """Update the component."""
+        for event in events:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if self.rect.collidepoint(event.pos):
+                    self.is_down = True
+            elif event.type == pygame.MOUSEBUTTONUP:
+                if self.is_down:
+                    self.is_down = False
+                    self.on_click()
+            elif event.type == pygame.MOUSEMOTION:
+                if self.is_down and not self.rect.collidepoint(event.pos):
+                    self.is_down = False
+
+
+def make_surface_rgba(array: numpy.typing.NDArray[numpy.uint8]):
+    """Returns a surface made from a [w, h, 4] numpy array with per-pixel alpha"""
+    shape = array.shape
+    if len(shape) != 3 and shape[2] != 4:
+        raise ValueError("Array not RGBA")
+
+    # Create a surface the same width and height as array and with per-pixel alpha.
+    surface = pygame.Surface(shape[0:2], pygame.SRCALPHA, 32)
+
+    # Copy the rgb part of array to the new surface.
+    pygame.pixelcopy.array_to_surface(surface, array[:,:,0:3])
+
+    # Copy the alpha part of array to the surface using a pixels-alpha view of the surface.
+    surface_alpha = numpy.array(surface.get_view('A'), copy=False)
+    surface_alpha[:,:] = array[:,:,3]
+
+    return surface
