64e09e079deb6dd30370e7907b5f3d928da5b2be
LioQing <46854695+LioQing@users.noreply.github.com>
2023-09-08 19:42:34 +0800

    Fix lint error

--- main/main.py
+++ main/main.py
@@ -4,8 +4,8 @@ import pygame
 
 from main.engine import Engine
 from main.screens.game import GameScreen
-from main.screens.main_menu import MainMenuScreen
 from main.screens.levels import LevelsScreen
+from main.screens.main_menu import MainMenuScreen
 from main.screens.test import TestScreen
 
 LOG_LEVEL = logging.DEBUG
